4e22e99e3794ec20bb055d2a0be7cb72a05f6f53
mb83pl <Doctype1983!>
2022-04-26 16:18:40 +0200

    appendChild

---
+++ script.js
@@ -0,0 +1,5 @@
+const containerElement = document.querySelector(".container");
+
+const element = document.createElement("div");
+element.innerHTML = "Hi there";
+containerElement.appendChild(element);
